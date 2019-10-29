5194d9f60b10d5ad6a811e0b337bba868a29560e
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2019-10-30 00:25:53 +0100

    Screen close.

--- src/main/java/xieao/countdown/client/gui/HudSettingScreen.java
+++ src/main/java/xieao/countdown/client/gui/HudSettingScreen.java
@@ -3,6 +3,7 @@ package xieao.countdown.client.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -56,5 +57,20 @@ public class HudSettingScreen extends Screen {
     public void onClose() {
         HudSettings.set(this.visiblity, this.cdX, this.cdY);
         super.onClose();
+    }
+
+    @Override
+    public boolean keyPressed(int p_keyPressed_1_, int p_keyPressed_2_, int p_keyPressed_3_) {
+        InputMappings.Input mouseKey = InputMappings.getInputByCode(p_keyPressed_1_, p_keyPressed_2_);
+        if (this.minecraft != null && (p_keyPressed_1_ == 256 || HudHandler.KEY.isPressed() || this.minecraft.gameSettings.keyBindInventory.isActiveAndMatches(mouseKey))) {
+            this.minecraft.player.closeScreen();
+            return true;
+        }
+        return super.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 }
